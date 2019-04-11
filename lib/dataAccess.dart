4314975a2097817714743a183f15602fe167ea68
yosindrax <ginsi_will@hotmail.com>
2019-04-11 13:58:55 +0200

    Add project files + working sqflite database
    State: Functional App, shows a list of items coming from a local sqlite database. Each item has a id, text + status. It is possible to change the status.

---
+++ lib/dataAccess.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'todoItem.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+final String todoTable = "TodoItems";
+
+class DataAccess {
+  static final DataAccess _instance = DataAccess._internal();
+  Database _db;
+
+  factory DataAccess() {
+    return _instance;
+  }
+
+  DataAccess._internal();
+
+  Future open() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'todo.db';
+
+    _db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute('''
+            create table $todoTable ( 
+            id integer primary key autoincrement, 
+            name text not null,
+            isComplete integer not null)
+            ''');
+        });
+    // This is just a convenience block to populate the database if it's empty.
+    // We likely wouldn't use this in a real application
+    if((await getTodoItems()).length == 0) {
+      insertTodo(TodoItem(name: "Test1 Task", isComplete: true));
+      insertTodo(TodoItem(name: "Test2 Task"));
+      insertTodo(TodoItem(name: "Test3 Task"));
+    }
+  }
+
+  Future<List<TodoItem>> getTodoItems() async {
+    var data = await _db.query(todoTable);
+    return data.map((d) => TodoItem.fromMap(d)).toList();
+  }
+
+  Future insertTodo(TodoItem item) {
+    return _db.insert(todoTable, item.toMap());
+  }
+}
---
+++ lib/todoListScreen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'todoItem.dart';
+import 'dart:async';
+import 'dataAccess.dart';
+
+class TodoListScreen extends StatefulWidget {
+  TodoListScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TodoListScreenState createState() => new _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<TodoItem> _todoItems = List();
+  DataAccess _dataAccess;
+
+  _TodoListScreenState() {
+    _dataAccess = DataAccess();
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _dataAccess.open().then((result) {
+      _dataAccess.getTodoItems()
+          .then((r) {
+        setState(() { _todoItems = r; });
+      });
+    });
+  }
+
+  void _addTodoItem() {
+    // TODO navigate to Create Todo Item Screen
+  }
+
+  void _updateTodoCompleteStatus(TodoItem item, bool newStatus) {
+    final tempTodoItems = _todoItems;
+    tempTodoItems.firstWhere((i) => i.id ==item.id).isComplete = newStatus;
+    setState(() { _todoItems = tempTodoItems; });
+    // TODO: Persist change
+  }
+
+  void _deleteTodoItem(TodoItem item) {
+    final tempTodoItems = _todoItems;
+    tempTodoItems.remove(item);
+    setState(() { _todoItems = tempTodoItems; });
+    // TODO: Persist change
+  }
+
+  Future<Null> _displayDeleteConfirmationDialog(TodoItem item) {
+    return showDialog<Null>(
+        context: context,
+        barrierDismissible: true, // Allow dismiss when tapping away from dialog
+        builder: (BuildContext context) {
+          return  AlertDialog(
+            title: Text("Delete TODO"),
+            content: Text("Do you want to delete \"${item.name}\"?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: Navigator.of(context).pop, // Close dialog
+              ),
+              FlatButton(
+                child: Text("Delete"),
+                onPressed: () {
+                  _deleteTodoItem(item);
+                  Navigator.of(context).pop(); // Close dialog
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  Widget _createTodoItemWidget(TodoItem item) {
+    return ListTile(
+      title: Text(item.name),
+      onLongPress: () => _displayDeleteConfirmationDialog(item),
+      trailing: Checkbox(
+        value: item.isComplete,
+        onChanged: (value) => _updateTodoCompleteStatus(item, value),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _todoItems.sort();
+    final todoItemWidgets = _todoItems.map(_createTodoItemWidget).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: todoItemWidgets,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodoItem,
+        tooltip: 'Add Todo',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
